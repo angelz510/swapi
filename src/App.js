2613f69625badd07d1b96c2315ab0c7f500a290e
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2022-07-21 14:29:43 -0700

    build(axios): makes call to recieve people

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const results = await axios(`https://swapi.dev/api/people`);
+
+      setItems(results.data);
+      console.log(results.data);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <div className="App">
       <Header />
